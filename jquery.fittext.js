d0b3043b6ff10b1c5e1fd8708b9ce923e8889c65
Brett Andrews <brett.j.andrews@gmail.com>
2016-04-19 13:07:06 -0700

    vanilla
    Needs logic to resize automatically on viewport change

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -1,43 +1,9 @@
-/*global jQuery */
-/*!
-* FitText.js 1.2
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/
+const DEFAULT_SETTINGS = {
+    minFontSize: Number.NEGATIVE_INFINITY,
+    maxFontSize: Number.POSITIVE_INFINITY
+}
 
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
+export default function(element, compressor = 1, settings) {
+    const finalSettings = Object.assign({}, DEFAULT_SETTINGS, settings)
+    element.style.fontSize = Math.max(Math.min(element.offsetWidth / (compressor*10), parseFloat(finalSettings.maxFontSize)), parseFloat(finalSettings.minFontSize))
+}
